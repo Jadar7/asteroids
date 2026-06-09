15708181269f7bcd0a1f3c674cc3f824cef05f37
Jadar7 <nhammond919@gmail.com>
2026-06-08 23:48:49 -0400

    Player movement added, bugs fixed. Added groups: updatable, drawable | CH3:L5

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,10 +24,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen, "black")
-            player.update(dt)
-            player.draw(screen)
-            pygame.display.flip()
+
+        updatable.update(dt)
+
+        pygame.Surface.fill(screen, "black")
+        for d in drawable:
+            d.draw(screen)
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
